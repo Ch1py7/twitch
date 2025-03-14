0ce182da8956d38683fc0b9ce3983ae563a7732d
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-03-14 03:27:00 -0600

    feat: add irc service for a ws communication, add message parser for better readability

---
+++ api/Services/IrcService.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using api.infrastructure.repositories.twitch;
+using api.Models;
+using api.infrastructure.parser;
+
+namespace api.Services
+{
+    public class IrcService
+    {
+        private byte[] buffer = new byte[4096];
+        private readonly ClientWebSocket ws;
+        private TwitchRepository twitchRepository;
+
+        public IrcService(ClientWebSocket ws, TwitchRepository twitchRepository)
+        {
+            this.ws = ws;
+            this.twitchRepository = twitchRepository;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message + "\r\n");
+            await ws.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        public async Task Listen(CancellationToken stoppingToken)
+        {
+            var buffer = new byte[8192];
+
+            while (ws.State == WebSocketState.Open && !stoppingToken.IsCancellationRequested)
+            {
+                var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), stoppingToken);
+                string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                MessageParser parsedMessages = new MessageParser(message);
+
+                if (message.Contains(":tmi.twitch.tv NOTICE * :Login authentication failed"))
+                {
+                    await this.twitchRepository.RefreshToken("");
+                }
+                else
+                {
+                    if (message.Contains("PING :tmi.twitch.tv"))
+                    {
+                        Console.WriteLine("receiving ping, sending pong");
+                        await SendMessage("PONG :tmi.twitch.tv");
+                    }
+                    else
+                    {
+                        if (!message.Contains("Welcome") && !message.Contains("JOIN") && !message.Contains("CAP * ACK") && !message.Contains("NAMES"))
+                        {
+                            TwitchMessage parsedToTwitch = parsedMessages.ToTwitchMessage();
+                            Console.WriteLine(parsedToTwitch.Channel);
+                            Console.WriteLine(parsedToTwitch.UserInfo.UserName);
+                            Console.WriteLine(parsedToTwitch.UserInfo.Color);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ api/infrastructure/parser/MessageParser.cs
@@ -0,0 +1,70 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+using api.Models;
+
+namespace api.infrastructure.parser
+{
+    public class MessageParser
+    {
+        private readonly Dictionary<string, string> parsedMessage;
+        private string messageType { get; set; } = "";
+        private readonly string rawMessage;
+
+        public MessageParser(string message)
+        {
+            this.rawMessage = message;
+            this.parsedMessage = message.Split(';')
+                .Where(msg => msg.Contains("="))
+                .Select(msg => msg.Split('='))
+                .Where(args => args.Length == 2)
+                .ToDictionary(args => args[0], args => args[1]);
+
+            this.messageType = this.DetectMessageType(message);
+        }
+        public TwitchMessage? ToTwitchMessage()
+        {
+            if (this.messageType != "PRIVMSG") return null;
+
+            var (channel, messageContent) = ExtractChannelAndMessage();
+
+            return new TwitchMessage
+            {
+                Date = parsedMessage.GetValueOrDefault("tmi-sent-ts", ""),
+                Channel = channel,
+                ChannelId = parsedMessage.GetValueOrDefault("room-id", ""),
+                FirstMsg = parsedMessage.GetValueOrDefault("first-msg", "0") == "1",
+                UserInfo = ExtractUserInfo(messageContent)
+            };
+        }
+        private string DetectMessageType(string message) => Regex.Match(message, @"tmi.twitch.tv\s*(\S+)").Groups[1].Value ?? "UNKNOWN";
+
+        private (string channel, string messageContent) ExtractChannelAndMessage()
+        {
+            var match = Regex.Match(this.rawMessage, @"PRIVMSG #(\S+) :(.*)");
+            if (match.Success)
+            {
+                return (match.Groups[1].Value, match.Groups[2].Value);
+            }
+            return ("", "");
+        }
+        private UserInfo ExtractUserInfo(string messageContent)
+        {
+            parsedMessage.TryGetValue("display-name", out string? displayName);
+            parsedMessage.TryGetValue("color", out string? color);
+            parsedMessage.TryGetValue("user-id", out string? userId);
+            parsedMessage.TryGetValue("@badge-info", out string? subscriberInfo);
+
+            return new UserInfo
+            {
+                DisplayName = displayName ?? "",
+                Color = color ?? "",
+                UserId = userId ?? "",
+                IsSubscriber = !String.IsNullOrEmpty(subscriberInfo) && subscriberInfo.Contains("subscriber"),
+                SubscriberMonths = !string.IsNullOrEmpty(subscriberInfo) && subscriberInfo.Contains("/")
+                                    ? subscriberInfo.Split("/").ElementAtOrDefault(1) ?? ""
+                                    : "",
+                Message = messageContent,
+            };
+        }
+    }
+}
