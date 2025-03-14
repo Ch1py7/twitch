034a2ddb66fc4eb43ec9cc8e75ec7df7cba4ffcb
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-03-14 03:23:38 -0600

    fix: set config file with default values

--- api/infrastructure/config/Config.cs
+++ api/infrastructure/config/Config.cs
@@ -2,11 +2,13 @@
 {
     public class Config
     {
-        public string ClientId { get; } = System.Environment.GetEnvironmentVariable("ClientId", EnvironmentVariableTarget.Machine) ?? "";
-        public string Secret { get; } = System.Environment.GetEnvironmentVariable("Secret", EnvironmentVariableTarget.Machine) ?? "";
+        public string ClientId { get; set; } = "";
+        public string Secret { get; set; } = "";
         public string GrantType { get; set; } = "client_credentials";
         public string RedirectUri { get; set; } = "http://localhost:3000";
-        public string Scope { get; set; } = "chat:read"; // %3A
+        public string Scope { get; set; } = "chat:read";
         public string TokenType { get; set; } = "bearer";
+        public string AccessToken { get; set; } = "";
+        public string RefreshToken { get; set; } = "";
     }
 }
