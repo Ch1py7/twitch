9f29d4694606fc9a298eea59054353256d791566
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-03-14 03:26:09 -0600

    feat: change irc client to wait for access token and refresh token to initialize it

--- api/infrastructure/irc/TwitchChat.cs
+++ api/infrastructure/irc/TwitchChat.cs
@@ -1,7 +1,8 @@
 ﻿using System.Net.WebSockets;
-using System.Text;
-using System.Text.Json;
-using System.Text.RegularExpressions;
+using api.infrastructure.config;
+using api.infrastructure.repositories.twitch;
+using api.Services;
+using Microsoft.Extensions.Options;
 
 namespace api.infrastructure.irc
 {
@@ -15,58 +16,37 @@ namespace api.infrastructure.irc
         private readonly ClientWebSocket ws = new();
         private readonly ILogger<TwitchChat> logger;
 
-        public TwitchChat(ILogger<TwitchChat> logger)
+        private readonly Config config;
+        private readonly IrcService ircService;
+
+        public TwitchChat(ILogger<TwitchChat> logger, TwitchRepository twitchRepository, IOptions<Config> config)
         {
+            this.config = config.Value;
             this.logger = logger;
+
+            this.ircService = new IrcService(this.ws, twitchRepository);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            while (string.IsNullOrEmpty(this.config.AccessToken) || string.IsNullOrEmpty(this.config.RefreshToken))
+            {
+                await Task.Delay(5000, stoppingToken);
+            }
+
+            logger.LogInformation("Available tokens. initialazing bot...");
+
             logger.LogInformation("connecting");
 
             await ws.ConnectAsync(new Uri(this.TWITCH_IRC_URL), stoppingToken);
             logger.LogInformation("connected");
 
-            await SendMessage($"PASS {this.TOKEN}");
-            await SendMessage($"NICK {this.NICKNAME}");
-            await SendMessage($"JOIN #{this.CHANNEL}");
-            await SendMessage("CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership");
+            await this.ircService.SendMessage($"PASS {this.TOKEN}{this.config.AccessToken}");
+            await this.ircService.SendMessage($"NICK {this.NICKNAME}");
+            await this.ircService.SendMessage($"JOIN #{this.CHANNEL}");
+            await this.ircService.SendMessage("CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership");
 
-            await Listen(stoppingToken);
-        }
-
-        private async Task SendMessage(string message)
-        {
-            var bytes = Encoding.UTF8.GetBytes(message + "\r\n");
-            await ws.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
-        }
-
-        private async Task Listen(CancellationToken stoppingToken)
-        {
-            var buffer = new byte[8192];
-
-            while (ws.State == WebSocketState.Open && !stoppingToken.IsCancellationRequested)
-            {
-                var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), stoppingToken);
-                string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                this.logger.LogInformation(message);
-
-                if (message.Contains("PING :tmi.twitch.tv"))
-                {
-                    Console.WriteLine("receiving ping, sending pong");
-                    await SendMessage("PONG :tmi.twitch.tv");
-                }
-
-                var match = Regex.Match(message, @"@.*;emotes=([^;]*);.*PRIVMSG #\w+ :(.*)");
-                if (match.Success)
-                {
-                    string username = match.Groups[1].Value;
-                    string text = match.Groups[2].Value;
-
-                    string jsonOutput = JsonSerializer.Serialize(message, new JsonSerializerOptions { WriteIndented = true });
-                    //this.logger.LogInformation($"JSON Message: {jsonOutput}");
-                }
-            }
+            await this.ircService.Listen(stoppingToken);
         }
     }
 }
